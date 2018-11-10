98eff585116c77317a3d3d3cc35cd9ad66159af5
Robert Selvek <selverob@fit.cvut.cz>
2018-11-10 15:57:41 +0100

    Add dragging functionality into grid.py

--- grid.py
+++ grid.py
@@ -2,61 +2,97 @@
 
 import arcade
 from arcade.color import BLACK, WHITE
-import networkx as nx
-import cProfile
 import level
 from sys import argv
 
+
 class Grid(arcade.Window):
-    __COLORS = [arcade.color.GREEN, arcade.color.BABY_BLUE, arcade.color.TEAL, arcade.color.MODE_BEIGE,
-                arcade.color.NAVY_BLUE, arcade.color.PURPLE, arcade.color.BROWN, arcade.color.GRAY]
+    __COLORS = [arcade.color.GREEN, arcade.color.BABY_BLUE, arcade.color.TEAL,
+                arcade.color.MODE_BEIGE, arcade.color.NAVY_BLUE,
+                arcade.color.PURPLE, arcade.color.BROWN, arcade.color.GRAY]
 
     def __init__(self, level_map, scenario, paths=None, cell_size=5, border=1):
         self.grid_size = (len(level_map), len(level_map[0]))
         self.cell_size = cell_size
         self.border = border
-        self.screen_size = (self.grid_size[1] * (cell_size + border) +
-                            border, self.grid_size[0] * (cell_size + border) + border)
-        self.background = self.__get_background(level_map, scenario.danger_coords(self.grid_size[1]))
-        if paths != None:
+        self.level_map = level_map
+        self.danger = set(scenario.danger_coords(self.grid_size[1]))
+        self.screen_size = (self.grid_size[1] * (cell_size + border) + border,
+                            self.grid_size[0] * (cell_size + border) + border)
+        self.__update_walls()
+        self.__update_danger()
+        if paths is not None:
             self.paths = paths
             self.path_idx = 0
             self.__update_agents()
         else:
             self.agents = None
+        self.current_drag_coords = None
         super().__init__(self.screen_size[0], self.screen_size[1])
         arcade.set_background_color(WHITE)
         self.set_update_rate(1/3)
 
     def on_draw(self):
         arcade.start_render()
-        self.background.draw()
-        if self.agents != None:
+        self.walls.draw()
+        self.danger_cells.draw()
+        if self.agents is not None:
             self.agents.draw()
 
-    def on_update(self, delta_time):
-        if self.agents != None:
+    def on_update(self, delta_time: float):
+        if self.agents is not None:
             self.path_idx += 1
             self.__update_agents()
-    
-    def on_mouse_press(self, x, y, button, modifiers):
+
+    def on_mouse_press(self, x: float,  y: float, button: int, modifiers: int):
+        if button == 1:
+            self.__update_drag_coords(x, y)
+        elif button == 4:
+            d = map(lambda x: level.coords_to_id(self.grid_size[1], *x),
+                    self.danger)
+            print(*sorted(d))
+
+    def on_mouse_drag(self, x: float, y: float, dx: float, dy: float, button: int, modifiers: int):
+        if button != 1:
+            return
+        self.__update_drag_coords(x, y)
+
+    def __update_drag_coords(self, x, y):
         row = self.grid_size[0] - y // (self.cell_size + self.border) - 1
         col = x // (self.cell_size + self.border)
-        print(row, col, level.coords_to_id(self.grid_size[1], row, col))
+        if (row, col) != self.current_drag_coords:
+            self.current_drag_coords = (row, col)
+            is_valid_row = row >= 0 and row < self.grid_size[0]
+            is_valid_col = col >= 0 and col < self.grid_size[1]
+            if is_valid_row and is_valid_col and self.level_map[row][col] != "@":
+                self.danger.add(self.current_drag_coords)
+                self.__update_danger()
 
-    def __get_background(self, level_map, danger):
-        bg = arcade.ShapeElementList()
-        for row, row_data in enumerate(level_map):
+    def __update_walls(self):
+        walls = arcade.ShapeElementList()
+        for row, row_data in enumerate(self.level_map):
             for col, character in enumerate(row_data):
                 if character == "@":
                     pos = self.__pos_for_coords(row, col)
-                    shape = arcade.create_rectangle_filled(pos[0], pos[1], self.cell_size, self.cell_size, BLACK)
-                    bg.append(shape)
-        for coords in danger:
+                    shape = arcade.create_rectangle_filled(pos[0],
+                                                           pos[1],
+                                                           self.cell_size,
+                                                           self.cell_size,
+                                                           BLACK)
+                    walls.append(shape)
+        self.walls = walls
+
+    def __update_danger(self):
+        danger_cells = arcade.ShapeElementList()
+        for coords in self.danger:
             pos = self.__pos_for_coords(coords[0], coords[1])
-            shape = arcade.create_rectangle_filled(pos[0], pos[1], self.cell_size, self.cell_size, arcade.color.RED)
-            bg.append(shape)
-        return bg
+            shape = arcade.create_rectangle_filled(pos[0],
+                                                   pos[1],
+                                                   self.cell_size,
+                                                   self.cell_size,
+                                                   arcade.color.RED)
+            danger_cells.append(shape)
+        self.danger_cells = danger_cells
 
     def __update_agents(self):
         node_ids = [path[self.path_idx] if self.path_idx < len(path) else path[-1] for path in self.paths]
@@ -65,23 +101,31 @@ class Grid(arcade.Window):
             coords = level.id_to_coords(self.grid_size[1], node_id)
             pos = self.__pos_for_coords(*coords)
             color = Grid.__COLORS[i % len(Grid.__COLORS)]
-            shape = arcade.create_rectangle_filled(pos[0], pos[1], self.cell_size, self.cell_size, color)
+            shape = arcade.create_rectangle_filled(pos[0],
+                                                   pos[1],
+                                                   self.cell_size,
+                                                   self.cell_size,
+                                                   color)
             self.agents.append(shape)
 
     def __pos_for_coords(self, row, col):
-        x = (col * (self.cell_size + self.border) + self.border + self.cell_size/2)
-        y = self.screen_size[1] - (row * (self.cell_size + self.border) + self.border + self.cell_size/2)
+        cell_with_border = (self.cell_size + self.border)
+        x = col * cell_with_border + self.border + self.cell_size/2
+        y = self.screen_size[1] - (
+            row * cell_with_border + self.border + self.cell_size/2)
         return (x, y)
 
-def parse_paths(path):
+
+def parse_paths(path: str):
     result = []
     with open(path) as f:
         lines = f.readlines()
-        if lines[-1] == [""]: lines = lines[:-1]
+        if lines[-1] == [""]:
+            lines = lines[:-1]
         for line in lines:
             result.append(list(map(lambda s: int(s), line.strip().split(" "))))
     return result
-        
+
 
 def main():
     map_path, scen_path = argv[1], argv[2]
@@ -90,7 +134,8 @@ def main():
         paths = parse_paths(argv[3])
     lines = []
     with open(map_path) as map_f:
-        for _ in range(4): map_f.readline()
+        for _ in range(4):
+            map_f.readline()
         lines = list(map(lambda l: l.strip(), map_f.readlines()))
     scenario = level.Scenario.from_file(scen_path)
     Grid(lines, scenario, paths, cell_size=10, border=2)
--- pashd.py
+++ pashd.py
@@ -1,36 +1,44 @@
 #! /usr/bin/env python3
 
 # PASHD - Per-agent Shortest Heurestic Distance
-# - Agent uses a heurestic (Manhattan distance) to generate a list of closest safe nodes
-# - Using A*, agent finds a path towards the closest one. If that's impossible, it sequentially tries next-best nodes
-# - Agent pathfinds to the goal, using LRA* (follows the planned route and in case of imminent collision, it replans)
-# - If the goal is already occupied, algorithm begins again, considering agent's current position as the start
+# - Agent uses a heurestic (Manhattan distance) to generate
+#   a list of closest safe nodes
+# - Using A*, agent finds a path towards the closest one.
+#   If that's impossible, it sequentially tries next-best nodes
+# - Agent pathfinds to the goal, using LRA*
+#   (follows the planned route and in case of imminent collision, it replans)
+# - If the goal is already occupied, algorithm begins again,
+#   considering agent's current position as the start
 
-from sys import argv
+import random
+from sys import argv, stderr
 from pqdict import pqdict
 from level import NoPathFound, Level
-import random
+
 
 class Agent:
-    def __init__(self, agent_id, level):
+    def __init__(self, agent_id: float, level: Level):
         self.id = agent_id
         self.level = level
         self.pos = level.scenario.agents[agent_id]
         self.retarget()
-    
+
     def retarget(self, first_move_disallow=set()):
         self.next_path_idx = 1
         self.path = None
-        goal_candidates = map(lambda x: (x, self.manhattan_distance(self.pos, x)), self.level.frontier)
+        goal_candidates = map(
+            lambda x: (x, self.manhattan_distance(self.pos, x)),
+            self.level.frontier)
         for candidate, _ in sorted(goal_candidates, key=lambda x: x[1]):
             # Currently, agents do not move away from occupied safe nodes so
             # occupied goals can be ignored outright
-            if candidate in first_move_disallow: continue
+            if candidate in first_move_disallow:
+                continue
             path = self.pathfind_to(candidate, first_move_disallow)
-            if path: 
+            if path:
                 self.path = path
                 return
-    
+
     def pathfind_to(self, goal, first_move_disallow=set()):
         g = self.level.g
         nan = float("nan")
@@ -46,15 +54,17 @@ class Agent:
                 return reconstruct_path(predecessors, goal)
             closed.add(curr)
             for n in g[curr].keys():
-                if n in closed: continue
-                if first_move and n in first_move_disallow: continue
+                if n in closed:
+                    continue
+                if first_move and n in first_move_disallow:
+                    continue
                 considered_g_cost = g_costs[curr] + 1
                 if considered_g_cost >= g_costs.get(n, nan):
                     continue
                 g_costs[n] = considered_g_cost
                 predecessors[n] = curr
                 f_cost = considered_g_cost + self.manhattan_distance(n, goal)
-                if not n in opened:
+                if n not in opened:
                     opened.additem(n, f_cost)
                 else:
                     opened[n] = f_cost
@@ -65,22 +75,23 @@ class Agent:
         x_coords = self.level.id_to_coords(x)
         y_coords = self.level.id_to_coords(y)
         return abs(x_coords[0] - y_coords[0]) + abs(x_coords[1] - y_coords[1])
-    
+
     def peek_move(self):
-        if self.path == None:
+        if self.path is not None:
             raise NoPathFound()
         if len(self.path) <= self.next_path_idx:
             return None
         return self.path[self.next_path_idx]
-    
+
     def do_move(self):
-        if self.path == None:
+        if self.path is not None:
             raise NoPathFound()
         if len(self.path) <= self.next_path_idx:
             raise StopIteration()
         self.pos = self.path[self.next_path_idx]
         self.next_path_idx += 1
         return self.pos
+
 
 def plan_evacuation(level, random_seed=42):
     random.seed(random_seed)
@@ -95,10 +106,11 @@ def plan_evacuation(level, random_seed=42):
         # Currently the simplest way to prevent zero-time swaps
         for i in agent_order:
             occupied.add(agents[i].pos)
-    
+
         for i in agent_order:
             next_pos = agents[i].peek_move()
-            if next_pos == None: continue
+            if next_pos is not None:
+                continue
             finished = False
             if next_pos in occupied:
                 agents[i].retarget(occupied)
@@ -106,7 +118,8 @@ def plan_evacuation(level, random_seed=42):
             agent_paths[i].append(next_pos)
             occupied.add(next_pos)
     return agent_paths
-                
+
+
 def reconstruct_path(predecessors, goal):
     path = [goal]
     while predecessors.get(path[-1], None):
@@ -114,12 +127,17 @@ def reconstruct_path(predecessors, goal):
     path.reverse()
     return path
 
+
 def main():
     map_path, scen_path = argv[1], argv[2]
     l = Level(map_path, scen_path)
+    if len(l.frontier) == 0:
+        print("No passage to safety exists!", file=stderr)
+        exit(2)
     paths = plan_evacuation(l)
     for path in paths:
         print(*path)
+
 
 if __name__ == "__main__":
     main()
