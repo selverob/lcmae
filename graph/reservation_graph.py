e3953184d3b39acf50a04bda71ceee8ad7dedfb4
Róbert Selvek <selverob@fit.cvut.cz>
2019-02-03 22:14:53 +0100

    Add forgotten graph changes

--- graph/reservation_graph.py
+++ graph/reservation_graph.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from graph.interface import Graph, Node
 from typing import List
 import networkx as nx
@@ -13,25 +14,43 @@ class ReservationNode(Node):
     def pos(self) -> int:
         return self._pos
 
+    def incremented_t(self) -> ReservationNode:
+        return ReservationNode(self.pos(), self.t + 1)
+
 class ReservationGraph(Graph[ReservationNode]):
     def __init__(self, underlying_graph: nx.Graph):
         self.g = underlying_graph
         for n in self.g.nodes:
-            self.g.nodes[n]["reservations"] = set()
+            self.g.nodes[n]["reservations"] = dict()
+            self.g.nodes[n]["occupied"] = dict()
     
     def neighbors(self, n: ReservationNode) -> List[ReservationNode]:
         neighbors = []
         for k in self.g[n.pos()].keys():
             rn = ReservationNode(k, n.t + 1)
-            if not self.is_reserved(rn):
+            if self.reserved_by(rn) is None:
                 neighbors.append(rn)
-        this_node = ReservationNode(n.pos(), n.t + 1)
-        if not self.is_reserved(this_node):
+        this_node = n.incremented_t()
+        if self.reserved_by(this_node) is None:
             neighbors.append(this_node)
         return neighbors
   
-    def is_reserved(self, n: ReservationNode) -> bool:
-        return n.t in self.g.nodes[n.pos()]["reservations"]
+    def reserved_by(self, n: ReservationNode) -> int:
+        return self.g.nodes[n.pos()]["reservations"].get(n.t)
     
-    def reserve(self, n: ReservationNode):
-        self.g.nodes[n.pos()]["reservations"].insert(n.t)
+    def reserve(self, n: ReservationNode, agent: int):
+        self.g.nodes[n.pos()]["reservations"][n.t] = agent
+    
+    def cancel_reservation(self, n: ReservationNode):
+        # We cannot be sure cancelled reservation will exist
+        # because agents reserve all of their locations for
+        # both t and t+1 and so if they're staying in the
+        # same node at both t and t+1, they'll try to cancel
+        # the reservation for t+1 twice
+        self.g.nodes[n.pos()]["reservations"].pop(n.t, None)
+
+    # def occupied_by(self, n: ReservationNode) -> int:
+    #     return n.t in self.g.nodes[n.pos()]["occupied"]
+    
+    # def occupy(self, n: ReservationNode):
+    #     self.g.nodes[n.pos()]["occupied"].insert(n.t)
