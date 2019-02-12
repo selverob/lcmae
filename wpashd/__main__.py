a187213341b7309520816dd6316ba75ed533d119
Róbert Selvek <selverob@fit.cvut.cz>
2019-02-12 11:12:25 +0100

    Improve file organization

---
+++ wpashd/__main__.py
@@ -0,0 +1,72 @@
+import random
+from sys import stderr, argv
+from graph.reservation_graph import ReservationGraph, Reservation, ReservationNode
+from wpashd.agent import Agent, LOOKAHEAD
+from level import Level
+
+def plan_evacuation(level, random_seed=42):
+    random.seed(random_seed)
+    reservations = ReservationGraph(level.g)
+    agents = [Agent(i, level, reservations) for i in range(len(level.scenario.agents))]
+    for agent in agents:
+        for i in range(LOOKAHEAD):
+            n = agent.pos.incremented_t(i)
+            reservations.reserve(Reservation(n, agent.id, 2))
+            agent.next_path.append(n)
+    safe = set([agent.id for agent in agents if agent.is_safe()])
+    endangered = set([agent.id for agent in agents if not agent.is_safe()])
+    # Time is watched independently by agents but this variable makes
+    # debugging easier
+    t = 0
+    deadlock_timer = 0
+    while deadlock_timer < 15:
+        print(f"Safe: {safe}", file=stderr)
+        print(f"Endangered: {endangered}", file=stderr)
+        deadlock_timer += 1
+        endangered_order = list(endangered)
+        newly_safe = []
+        newly_endangered = []
+        random.shuffle(endangered_order)
+        for i in endangered_order:
+            agents[i].step()
+            if len(agents[i].taken_path) < 2 or agents[i].taken_path[-1].pos() != agents[i].taken_path[-2].pos():
+                deadlock_timer = 0
+            if agents[i].is_safe():
+                newly_safe.append(i)
+        safe_order = list(safe)
+        random.shuffle(safe_order)
+        for i in safe_order:
+            agents[i].step()
+            if len(agents[i].taken_path) < 2 or agents[i].taken_path[-1].pos() != agents[i].taken_path[-2].pos():
+                deadlock_timer = 0
+            if not agents[i].is_safe():
+                newly_endangered.append(i)
+        for agent in newly_safe:
+            endangered.remove(agent)
+            safe.add(agent)
+        for agent in newly_endangered:
+            safe.remove(agent)
+            endangered.add(agent)
+        t += 1
+    return [list(map(ReservationNode.pos, agent.taken_path)) for agent in agents]
+
+
+def main():
+    map_path, scen_path = argv[1], argv[2]
+    lvl = Level(map_path, scen_path)
+    for n in lvl.g.nodes:
+        lvl.g.nodes[n]["reservations"] = set()
+    if len(lvl.frontier) == 0:
+        print("No passage to safety exists!", file=stderr)
+        exit(2)
+    paths = plan_evacuation(lvl)
+    for agent_id, path in enumerate(paths):
+        num_strings = ["{:02d}".format(n) for n in path]
+        print(*num_strings)
+        if path[-1] in lvl.scenario.danger:
+            print("Agent", agent_id, "could not evacuate. He dead.",
+                  file=stderr)
+
+
+if __name__ == "__main__":
+    main()
--- wpashd/agent.py
+++ wpashd/agent.py
@@ -16,12 +16,12 @@ from pqdict import pqdict
 from sys import argv, stderr
 import typing
 
-from w_astar import WindowedAstar
-from closest_frontier import ClosestFrontierFinder
 from graph.nx_graph import NxNode
 from graph.reservation_graph import ReservationGraph, ReservationNode, Reservation
 from level import Level
-from rra import RRAHeuristic
+from wpashd.closest_frontier import ClosestFrontierFinder
+from wpashd.rra import RRAHeuristic
+from wpashd.w_astar import WindowedAstar
 
 LOOKAHEAD = 10
 
@@ -111,69 +111,3 @@ class Agent:
     def _log(self, msg):
         print(f"a={self.id} t={self.taken_path[-1].t}: {msg}", file=stderr)
 
-def plan_evacuation(level, random_seed=42):
-    random.seed(random_seed)
-    reservations = ReservationGraph(level.g)
-    agents = [Agent(i, level, reservations) for i in range(len(level.scenario.agents))]
-    for agent in agents:
-        for i in range(LOOKAHEAD):
-            n = agent.pos.incremented_t(i)
-            reservations.reserve(Reservation(n, agent.id, 2))
-            agent.next_path.append(n)
-    safe = set([agent.id for agent in agents if agent.is_safe()])
-    endangered = set([agent.id for agent in agents if not agent.is_safe()])
-    # Time is watched independently by agents but this variable makes
-    # debugging easier
-    t = 0
-    deadlock_timer = 0
-    while deadlock_timer < 15:
-        print(f"Safe: {safe}", file=stderr)
-        print(f"Endangered: {endangered}", file=stderr)
-        deadlock_timer += 1
-        endangered_order = list(endangered)
-        newly_safe = []
-        newly_endangered = []
-        random.shuffle(endangered_order)
-        for i in endangered_order:
-            agents[i].step()
-            if len(agents[i].taken_path) < 2 or agents[i].taken_path[-1].pos() != agents[i].taken_path[-2].pos():
-                deadlock_timer = 0
-            if agents[i].is_safe():
-                newly_safe.append(i)
-        safe_order = list(safe)
-        random.shuffle(safe_order)
-        for i in safe_order:
-            agents[i].step()
-            if len(agents[i].taken_path) < 2 or agents[i].taken_path[-1].pos() != agents[i].taken_path[-2].pos():
-                deadlock_timer = 0
-            if not agents[i].is_safe():
-                newly_endangered.append(i)
-        for agent in newly_safe:
-            endangered.remove(agent)
-            safe.add(agent)
-        for agent in newly_endangered:
-            safe.remove(agent)
-            endangered.add(agent)
-        t += 1
-    return [list(map(ReservationNode.pos, agent.taken_path)) for agent in agents]
-
-
-def main():
-    map_path, scen_path = argv[1], argv[2]
-    lvl = Level(map_path, scen_path)
-    for n in lvl.g.nodes:
-        lvl.g.nodes[n]["reservations"] = set()
-    if len(lvl.frontier) == 0:
-        print("No passage to safety exists!", file=stderr)
-        exit(2)
-    paths = plan_evacuation(lvl)
-    for agent_id, path in enumerate(paths):
-        num_strings = ["{:02d}".format(n) for n in path]
-        print(*num_strings)
-        if path[-1] in lvl.scenario.danger:
-            print("Agent", agent_id, "could not evacuate. He dead.",
-                  file=stderr)
-
-
-if __name__ == "__main__":
-    main()
