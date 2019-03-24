4e748b68c5bedd27a6b41bf13a087aa9b6267e2b
Róbert Selvek <selverob@fit.cvut.cz>
2019-03-24 11:08:04 +0100

    Generate plots for the whole map too

--- helpers/generate_plots.py
+++ helpers/generate_plots.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import pathlib as pl
+from typing import Iterator, List, Tuple
+from itertools import groupby
 from os import listdir
 from sys import argv
 import matplotlib.pyplot as plt
@@ -13,10 +15,13 @@ AGENT_COLORS = {
     "PANICKED": "red"
 }
 
+LINE_STYLES = ['solid', 'dashed', 'dashdot', 'dotted']
 
-def to_percentages(df: pd.DataFrame):
+def to_percentages(df: pd.DataFrame) -> pd.DataFrame:
+    new_df = pd.DataFrame(columns=df.columns)
     for col in df.columns:
-        df[col] = df[col] * 100
+        new_df[col] = df[col] * 100
+    return new_df
 
 
 def make_plot(title: str, df: pd.DataFrame):
@@ -31,14 +36,51 @@ def make_plot(title: str, df: pd.DataFrame):
         plt.legend()
 
 
+def chart_groups(elems: Iterator[pl.PurePath]) -> List[Tuple[str, List[pl.PurePath]]]:
+    res = []
+    filtered = filter(lambda p: p.suffix == ".csv", elems)
+    for m, charts in groupby(filtered, lambda s: s.name.split(".")[0]):
+        res.append((m, list(charts)))
+    return res
+
+
+def single_run_plot(path: pl.PurePath, percentages: pd.DataFrame):
+    with PdfPages(path.with_suffix(".pdf")) as pp:
+        make_plot(path.stem.replace("-", " "), percentages)
+        pp.savefig()
+        plt.close()
+
+
+def group_plot(path: pl.PurePath, stems: List[str], percentages: List[pd.DataFrame]):
+    plt.figure()
+    plt.title(path.stem)
+    plt.xlabel("Time")
+    plt.ylabel("Safe agents")
+    plt.yticks(range(0, 101, 10))
+    plt.grid(True)
+    with PdfPages(path) as pp:
+        for i, df in enumerate(percentages):
+            for col in df:
+                line_name = stems[i].split("_")[-1].split(".")[0]
+                plt.plot(df[col],
+                         color=AGENT_COLORS[col.strip()],
+                         label=f"{line_name} - {col.strip().lower()}",
+                         linestyle=LINE_STYLES[i % len(LINE_STYLES)])
+        plt.legend()
+        pp.savefig()
+        plt.close()
+
+
+def process_group(group: Tuple[str, List[pl.PurePath]]):
+    percentages = [to_percentages(pd.read_csv(p)) for p in group[1]]
+    for path, percentage in zip(group[1], percentages):
+        single_run_plot(path, percentage)
+    group_plot(group[1][0].with_name(f"{group[0]}.pdf"), [p.stem for p in group[1]], percentages)
+
+
 if __name__ == "__main__":
     dir_path = pl.PurePath(argv[1])
-    for file in listdir(dir_path):
-        file_path = dir_path.joinpath(file)
-        if file_path.suffix == ".csv":
-            df = pd.read_csv(file_path)
-            to_percentages(df)
-            with PdfPages(file_path.with_suffix(".pdf")) as pp:
-                make_plot(file_path.stem.replace("-", " "), df)
-                pp.savefig()
-                plt.close()
+    file_paths = map(dir_path.joinpath, listdir(dir_path))
+    groups = chart_groups(file_paths)
+    for g in groups:
+        process_group(g)
