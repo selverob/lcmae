9d808353bd8566c78b927d8125c51f3e76c3c1e3
Róbert Selvek <selverob@fit.cvut.cz>
2019-04-24 10:59:16 +0200

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ setup(
     description="A simulator for evacuation planning algorithms",
     python_requires=">=3.7.0",
     packages=find_packages(),
-    install_requires=[
-        'Click',
-    ],
+    install_requires=[dep.strip() for dep in open("requirements.txt").readlines()],
     entry_points='''
         [console_scripts]
         evacsim=evacsim.cli:cli
